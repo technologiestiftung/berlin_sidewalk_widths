b5807fcd3d26b0c591c4c293b3476c9794a8d8d5
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2020-07-29 10:47:46 +0200

    chore: apply prettier

--- src/styles/Theme.js
+++ src/styles/Theme.js
@@ -1,15 +1,20 @@
 // example base theme from @theme-ui/presets
 export default {
   boxShadow: `0 2px 40px 0 rgba(30,55,145,0.15)`,
-  transitions: ['.125s all ease-in-out', '.250s all ease-in-out', '.375s all ease-in-out', '.5s all ease-in-out'],
-  borderLink: '1px solid black',
+  transitions: [
+    ".125s all ease-in-out",
+    ".250s all ease-in-out",
+    ".375s all ease-in-out",
+    ".5s all ease-in-out",
+  ],
+  borderLink: "1px solid black",
   // sx properties:
-  breakpoints: ['40em', '52em', '64em'],
+  breakpoints: ["40em", "52em", "64em"],
   space: [0, 4, 8, 12, 16, 32, 64, 128, 256, 512],
   sizes: [10, 20, 30, 40, 50, 60],
   fonts: {
-    body: 'Clan Book',
-    heading: 'Clan Medium',
+    body: "Clan Book",
+    heading: "Clan Medium",
   },
   fontSizes: [10, 13, 16, 20, 24, 32, 48, 64, 96],
   fontWeights: {
@@ -22,75 +27,75 @@ export default {
     heading: 1.125,
   },
   colors: {
-    text: '#000',
-    background: '#fff',
-    primary: '#07c',
-    secondary: '#30c',
-    muted: '#f6f6f6',
+    text: "#000",
+    background: "#fff",
+    primary: "#07c",
+    secondary: "#30c",
+    muted: "#f6f6f6",
     lightgrey: "#f5f5f5",
     midgrey: "#d8d8d8",
     textgrey: "#343434",
   },
   text: {
     heading: {
-      fontFamily: 'heading',
-      lineHeight: 'heading',
-      fontWeight: 'heading',
+      fontFamily: "heading",
+      lineHeight: "heading",
+      fontWeight: "heading",
     },
   },
   styles: {
     root: {
-      fontFamily: 'body',
-      lineHeight: 'body',
-      fontWeight: 'body',
+      fontFamily: "body",
+      lineHeight: "body",
+      fontWeight: "body",
     },
     h1: {
-      variant: 'text.heading',
+      variant: "text.heading",
       fontSize: 5,
     },
     h2: {
-      variant: 'text.heading',
+      variant: "text.heading",
       fontSize: 4,
     },
     h3: {
-      variant: 'text.heading',
+      variant: "text.heading",
       fontSize: 3,
     },
     h4: {
-      variant: 'text.heading',
+      variant: "text.heading",
       fontSize: 2,
     },
     h5: {
-      variant: 'text.heading',
+      variant: "text.heading",
       fontSize: 1,
     },
     h6: {
-      variant: 'text.heading',
+      variant: "text.heading",
       fontSize: 0,
     },
     pre: {
-      fontFamily: 'monospace',
-      overflowX: 'auto',
+      fontFamily: "monospace",
+      overflowX: "auto",
       code: {
-        color: 'inherit',
+        color: "inherit",
       },
     },
     code: {
-      fontFamily: 'monospace',
-      fontSize: 'inherit',
+      fontFamily: "monospace",
+      fontSize: "inherit",
     },
     table: {
-      width: '100%',
-      borderCollapse: 'separate',
+      width: "100%",
+      borderCollapse: "separate",
       borderSpacing: 0,
     },
     th: {
-      textAlign: 'left',
-      borderBottomStyle: 'solid',
+      textAlign: "left",
+      borderBottomStyle: "solid",
     },
     td: {
-      textAlign: 'left',
-      borderBottomStyle: 'solid',
+      textAlign: "left",
+      borderBottomStyle: "solid",
     },
   },
-}
+};
--- src/utils/index.js
+++ src/utils/index.js
@@ -3,11 +3,9 @@ export const createMarkup = (content) => {
 };
 
 export const id = () => {
-  return (
-    Math.random()
-      .toString(36)
-      .substr(2, 9)
-  );
+  return Math.random()
+    .toString(36)
+    .substr(2, 9);
 };
 
 export default {
